c34ed6be9300a3dc80e31e27e624ca41f73b9b65
Lucky  Odi <luckyodi310@gmail.com>
2026-01-25 13:08:25 +0100

    Remove import statement that are not of us anymore

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,9 +1,8 @@
 from flask import render_template, Blueprint
 from datetime import datetime
-from flask_login import current_user, login_required
 
 # Create blueprint
-main_bp = Blueprint('main', __name__)
+main_bp = Blueprint("main", __name__)
 
 @main_bp.context_processor
 def inject_now():
