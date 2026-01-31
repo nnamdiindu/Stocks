03bdffc4ccaef4e44b9ccb437cd64f899615a6be
Lucky  Odi <luckyodi310@gmail.com>
2026-01-31 23:33:24 +0100

    Add config file for db

---
+++ app/models/__init__.py
@@ -0,0 +1,9 @@
+from app.models.user import User
+from app.models.notification import Notification, NotificationPreference
+# Import other models as you create them
+
+__all__ = [
+    'User',
+    'Notification',
+    'NotificationPreference',
+]
