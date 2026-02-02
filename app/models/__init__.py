3808b8105222d657279ec5df3aa51cb275f2da60
Lucky  Odi <luckyodi310@gmail.com>
2026-02-02 17:36:02 +0100

    Modify config file

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,7 +3,7 @@ from app.models.notification import Notification, NotificationPreference
 # Import other models as you create them
 
 __all__ = [
-    'User',
-    'Notification',
-    'NotificationPreference',
+    "User",
+    "Notification",
+    "NotificationPreference",
 ]
