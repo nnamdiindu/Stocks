37d55c2c9af3cb96ee1e93528a581b559a8635c7
Lucky  Odi <luckyodi310@gmail.com>
2026-02-20 22:49:47 +0100

    Add more users comments

--- app/static/js/testimonials.js
+++ app/static/js/testimonials.js
@@ -3,43 +3,57 @@ const testimonials = [
     name: "David Tyler",
     location: "London, UK",
     rating: 4.8,
-    text: "StocksCo helped me start investing without stress. I now buy U.S. stocks weekly with small amounts, and the app's insights make everything easy to understand.",
+    text: "StocksCo helped me start investing without stress. I now buy U.S. stocks weekly with small amounts, and the insights make everything easy to understand.",
     image: "../static/images/david.png"
   },
   {
     name: "Kathleen Perry",
     location: "Austin, TX",
-    rating: 4.8,
+    rating: 4.5,
     text: "What I love most is the speed and transparency. Funding my account takes seconds, and withdrawals hit my bank quickly.",
     image: "../static/images/kathleen.png"
   },
   {
     name: "Kelvin M.",
     location: "Chicago, Illinois",
-    rating: 4.8,
+    rating: 4.3,
     text: "Security was my biggest concern, but StocksCo gives me peace of mind. The 2FA and real-time notifications assure me that my money is protected.",
     image: "../static/images/kelvin.png"
   }, 
   {
-    name: "John M.",
+    name: "John Wilfred",
     location: "Miami, Florida",
-    rating: 4.8,
-    text: "I've tried multiple platforms, but StocksCo stands out for its simple interface and clear pricing. No hidden fees, just honest investing.",
+    rating: 4.7,
+    text: "The interface is clean and beginner-friendly, yet powerful enough for serious investing. I especially appreciate the real-time charts and portfolio tracking tools.",
     image: "../static/images/john.png"
   },
   {
     name: "John D.",
     location: "Las Vegas, NV",
-    rating: 4.8,
-    text: "StocksCo helped me start investing without stress. I now buy U.S. stocks weekly with small amounts, and the app's insights make everything easy to understand.",
+    rating: 4.1,
+    text: "I started with a small amount just to test the platform, and I was impressed. Everything from account setup to placing my first trade was smooth and straightforward.",
     image: "../static/images/johnD.png"
   },
   {
-    name: "Martin Westwood",
-    location: "Las Vegas, NV",
-    rating: 4.8,
-    text: "StocksCo helped me start investing without stress. I now buy U.S. stocks weekly with small amounts, and the app's insights make everything easy to understand.",
-    image: "../static/images/johnD.png"
+    name: "Kimberly Westwood",
+    location: "New York City",
+    rating: 4.6,
+    text: "I like how easy it is to diversify my investments. Buying fractional shares allows me to invest in big companies without needing a large upfront budget.",
+    image: "../static/images/kimberly.jpeg"
+  },
+  {
+    name: "Sarah McLoughlin",
+    location: "Coventry, UK",
+    rating: 4.2,
+    text: "What stands out for me is the research tools. I can review stock performance, analyst insights, and trends all in one place without feeling overwhelmed.",
+    image: "../static/images/sarah.jpeg"
+  },
+  {
+    name: "Louis Bedford",
+    location: "Liverpool, UK",
+    rating: 4.9,
+    text: "StocksCo combines speed, reliability, and clarity. Whether I'm checking performance or making a quick trade, everything works seamlessly every time.",
+    image: "../static/images/louis.jpeg"
   },
 ]
 const duplicatedTestimonials = [...testimonials, ...testimonials];
