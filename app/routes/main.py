79ed1aca24fc6463df954fbcd8f3e62c0ead074e
Lucky  Odi <luckyodi310@gmail.com>
2026-02-21 14:50:53 +0100

    Add POST method to contact_us endpoint to receive data from form submission

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, flash, redirect, url_for
 from datetime import datetime
 
 # Create blueprint
@@ -23,8 +23,17 @@ def about_us():
 def features():
     return render_template("landing/features.html")
 
-@main_bp.route("/contact-us")
+@main_bp.route("/contact-us", methods=["GET", "POST"])
 def contact_us():
+    if request.method == "POST":
+        first_name = request.form.get("first_name")
+        phone_number = request.form.get("tel")
+        email = request.form.get("email")
+        message = request.form.get("message")
+        flash("Your message has been sent successfully.", "success")
+
+        print(first_name, phone_number, email, message)
+        return redirect(url_for("main.contact_us"))
     return render_template("landing/contact-us.html")
 
 @main_bp.route("/terms&conditions")
